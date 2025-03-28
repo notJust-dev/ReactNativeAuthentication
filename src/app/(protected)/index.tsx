a86e1d2fcf2ad6a219ab625d84e4b21a4e3d7b95
Vadim Savin <savinvadim1312@gmail.com>
2025-03-28 19:43:05 +0100

    profile screen

--- src/app/(protected)/index.tsx
+++ src/app/(protected)/index.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
+import { useAuth, useUser } from '@clerk/clerk-expo';
 
 export default function HomeScreen() {
+  const { user } = useUser();
+
+  const { signOut } = useAuth();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Home screen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 15,
+      }}
+    >
+      <Image
+        source={{ uri: user?.imageUrl }}
+        style={{ height: 100, aspectRatio: 1, borderRadius: 100 }}
+      />
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+        Welcome, {user?.firstName}
+      </Text>
+
       <Text style={{ fontSize: 18 }}>Only logged in users can see this</Text>
+
+      <Button title='Sign out' onPress={() => signOut()} />
     </View>
   );
 }
