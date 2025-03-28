f5bcccc0d1501f8910a2a6e8df412c3bd648b12d
Vadim Savin <savinvadim1312@gmail.com>
2025-03-28 16:43:44 +0100

    React Hook Form

--- App.tsx
+++ App.tsx
@@ -2,14 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import {
   StyleSheet,
   Text,
-  Pressable,
   KeyboardAvoidingView,
   Platform,
+  View,
 } from 'react-native';
 import CustomInput from './src/components/CustomInput';
 import CustomButton from './src/components/CustomButton';
 
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const signInSchema = z.object({
+  email: z.string({ message: 'Email is required' }).email('Invalid email'),
+  password: z
+    .string({ message: 'Password is required' })
+    .min(8, 'Password should be at least 8 characters long'),
+});
+
+type SignInFields = z.infer<typeof signInSchema>;
+
 export default function App() {
+  const { control, handleSubmit } = useForm({
+    resolver: zodResolver(signInSchema),
+  });
+
+  const onSignIn = (data: SignInFields) => {
+    // manual validation
+
+    console.log('Sign in: ', data.email, data.password);
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -17,22 +40,26 @@ export default function App() {
     >
       <Text style={styles.title}>Sign in</Text>
 
-      <CustomInput
-        placeholder='Email'
-        autoFocus
-        autoCapitalize='none'
-        keyboardType='email-address'
-        autoComplete='email'
-      />
+      <View style={styles.form}>
+        <CustomInput
+          control={control}
+          name='email'
+          placeholder='Email'
+          autoFocus
+          autoCapitalize='none'
+          keyboardType='email-address'
+          autoComplete='email'
+        />
 
-      <CustomInput placeholder='Password' secureTextEntry />
+        <CustomInput
+          control={control}
+          name='password'
+          placeholder='Password'
+          secureTextEntry
+        />
+      </View>
 
-      <CustomButton
-        text='Sign in'
-        onPress={() => {
-          console.log('Pressed');
-        }}
-      />
+      <CustomButton text='Sign in' onPress={handleSubmit(onSignIn)} />
 
       <StatusBar style='auto' />
     </KeyboardAvoidingView>
@@ -46,6 +73,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20,
     gap: 20,
+  },
+  form: {
+    gap: 5,
   },
   title: {
     fontSize: 24,
--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,18 +1,56 @@
-import { TextInput, StyleSheet, TextInputProps } from 'react-native';
+import {
+  TextInput,
+  StyleSheet,
+  TextInputProps,
+  Text,
+  View,
+} from 'react-native';
+import { Controller } from 'react-hook-form';
 
 type CustomInputProps = {
-  // custom fields
+  control: any; // custom fields
+  name: string;
 } & TextInputProps;
 
-export default function CustomInput(props: CustomInputProps) {
-  return <TextInput {...props} style={[styles.input, props.style]} />;
+export default function CustomInput({
+  control,
+  name,
+  ...props
+}: CustomInputProps) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({
+        field: { value, onChange, onBlur },
+        fieldState: { error },
+      }) => (
+        <View style={styles.container}>
+          <TextInput
+            {...props}
+            value={value}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            style={[styles.input, props.style]}
+          />
+          <Text style={styles.error}>{error?.message}</Text>
+        </View>
+      )}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    gap: 4,
+  },
   input: {
     borderWidth: 1,
     padding: 10,
     borderRadius: 5,
     borderColor: '#ccc',
+  },
+  error: {
+    color: 'crimson',
   },
 });
